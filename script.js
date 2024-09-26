438e3cd878c309c9668ea33902563fcb8954d404
Sharma7827 <sharmas4063@gmail.com>
2024-09-26 08:37:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+window.addEventListener('resize', updateScreenSize)
+
+function updateScreenSize() {
+    const width = window.innerWidth
+    const height = window.innerHeight
+    document.getElementById('width').textContent = width
+    document.getElementById('height').textContent = height
+  }
